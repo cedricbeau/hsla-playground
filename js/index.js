78b788e45733f57cfdc7a8b1b75f7ad514fc6a5a
Cedric Beau <cedric7693@hotmail.fr>
2020-05-23 11:13:40 +0200

    Refactor JavaScript

--- js/index.js
+++ js/index.js
@@ -1,127 +1,6 @@
 // -----------------------------------------------------------------------
-// GET HSLA
-// -----------------------------------------------------------------------
-/*
-let hslaDef = function (value1, value2, value3, value4, range1, range2, range3,range4, primary, complementary, triadic) {
-
-  let H = parseInt(range1)
-      S = parseInt(range2),
-      L = parseInt(range3),
-      A = range4,
-      complementaryH = H + 180 - 360,
-      triadicH = H + 120 - 360
-
-  value1.innerText = H;
-  value2.innerText = S + "%";
-  value3.innerText = L + "%";
-  value4.innerText = A;
-
-  // calc complementary
-  function calcComplementary() {
-    if(complementaryH < 0) {
-      return complementaryH + 360
-    } else {
-      return complementaryH
-    }
-  }
-
-  // calc triadic
-  function calcTriadic() {
-    if(triadicH < 0) {
-      return triadicH + 360
-    } else {
-      return triadicH
-    }
-  }
-
-  // set primary
-  primary.style.background = "hsla(" + H + ", " + S + "%, " + L + "%, " + A + ")";
-  primary.innerHTML = "<div class='result_primary'>" +
-  "<div class='color_title'><strong>Primary</strong></div>" +
-    "<div class='color_result'>" +
-      "<div class='color_result_item h'>" +
-        "<div class='color_result_value'>" + H + "</div> " +
-        "<div class='color_result_def'>H</div> " +
-      "</div>" +
-      "<div class='color_result_item s'>" +
-        "<div class='color_result_value'>" + S + "%</div> " +
-        "<div class='color_result_def'>S</div> " +
-      "</div>" +
-      "<div class='color_result_item l'>" +
-        "<div class='color_result_value'>" + L + "%</div> " +
-        "<div class='color_result_def'>L</div> " +
-      "</div>" +
-      "<div class='color_result_item a'>" +
-        "<div class='color_result_value'>" + A + "</div> " +
-        "<div class='color_result_def'>A</div> " +
-      "</div>" +
-    "</div>" +
-  "</div>";
-
-  // set complementary
-  complementary.style.background = "hsla(" + calcComplementary() + ", " + S + "%, " + L + "%, " + A + ")";
-  complementary.innerHTML = "<div class='result_complementary'>" +
-  "<div class='color_title'><strong>Complementary</strong></div>" +
-    "<div class='color_result'>" +
-      "<div class='color_result_item h'>" +
-        "<div class='color_result_value'>" + calcComplementary() + "</div> " +
-        "<div class='color_result_def'>H</div> " +
-      "</div>" +
-      "<div class='color_result_item s'>" +
-        "<div class='color_result_value'>" + S + "%</div> " +
-        "<div class='color_result_def'>S</div> " +
-      "</div>" +
-      "<div class='color_result_item l'>" +
-        "<div class='color_result_value'>" + L + "%</div> " +
-        "<div class='color_result_def'>L</div> " +
-      "</div>" +
-      "<div class='color_result_item a'>" +
-        "<div class='color_result_value'>" + A + "</div> " +
-        "<div class='color_result_def'>A</div> " +
-      "</div>" +
-    "</div>" +
-  "</div>";
-
-  // set triadic
-  triadic.style.background = "hsla(" + calcTriadic() + ", " + S + "%, " + L + "%, " + A + ")";
-  triadic.innerHTML = "<div class='result_complementary'>" +
-  "<div class='color_title'><strong>Triadic</strong></div>" +
-    "<div class='color_result'>" +
-      "<div class='color_result_item h'>" +
-        "<div class='color_result_value'>" + calcTriadic() + "</div> " +
-        "<div class='color_result_def'>H</div> " +
-      "</div>" +
-      "<div class='color_result_item s'>" +
-        "<div class='color_result_value'>" + S + "%</div> " +
-        "<div class='color_result_def'>S</div> " +
-      "</div>" +
-      "<div class='color_result_item l'>" +
-        "<div class='color_result_value'>" + L + "%</div> " +
-        "<div class='color_result_def'>L</div> " +
-      "</div>" +
-      "<div class='color_result_item a'>" +
-        "<div class='color_result_value'>" + A + "</div> " +
-        "<div class='color_result_def'>A</div> " +
-      "</div>" +
-    "</div>" +
-  "</div>";
-
-}
-
-let hslaDefFunc = function () {
-  let ranges = document.querySelectorAll(".selector_range")
-  let values = document.querySelectorAll(".selector_value")
-  let primary = document.querySelector('.primary')
-  let complementary = document.querySelector('.complementary')
-  let triadic = document.querySelector('.triadic')
-  hslaDef(values[0], values[1], values[2], values[3], ranges[0].value, ranges[1].value, ranges[2].value, ranges[3].value, primary,complementary,triadic)
-}
-*/
-// -----------------------------------------------------------------------
 // DISPLAY DYNAMIC VALUES
 // -----------------------------------------------------------------------
-
-
 
 var hslaPlayground = {
 
@@ -146,34 +25,50 @@ var hslaPlayground = {
     var H = parseInt(this.ranges[0].value)
         S = parseInt(this.ranges[1].value),
         L = parseInt(this.ranges[2].value),
-        A = this.ranges[3].value,
-        complementaryH = H + 180 - 360,
-        triadicH = H + 120 - 360;
+        A = this.ranges[3].value;
 
     this.values[0].innerText = H;
     this.values[1].innerText = S + "%";
     this.values[2].innerText = L + "%";
     this.values[3].innerText = A;
 
-    // calc complementary
-    function calcComplementary() {
-      if(complementaryH < 0) {
-        return complementaryH + 360
-      } else {
-        return complementaryH
-      }
-    }
+    this._displayPrimary(H, S, L, A)
+    this._displayComplementary(H, S, L, A)
+    this._displayTriadic(H, S, L, A)
+  },
 
-    // calc triadic
-    function calcTriadic() {
-      if(triadicH < 0) {
-        return triadicH + 360
-      } else {
-        return triadicH
-      }
+  /**
+   *
+   * @param {*} color
+   */
+  _calcComplementary: function(color) {
+    if(color < 0) {
+      return color + 360
+    } else {
+      return color
     }
+  },
 
-    // set primary
+  /**
+   *
+   * @param {*} color
+   */
+  _calcTriadic: function(color) {
+    if(color < 0) {
+      return color + 360
+    } else {
+      return color
+    }
+  },
+
+  /**
+   *
+   * @param {*} H
+   * @param {*} S
+   * @param {*} L
+   * @param {*} A
+   */
+  _displayPrimary: function(H, S, L, A) {
     this.primary.style.background = "hsla(" + H + ", " + S + "%, " + L + "%, " + A + ")";
     this.primary.innerHTML = "<div class='result_primary'>" +
     "<div class='color_title'><strong>Primary</strong></div>" +
@@ -196,14 +91,24 @@ var hslaPlayground = {
         "</div>" +
       "</div>" +
     "</div>";
+  },
 
-    // set complementary
-    this.complementary.style.background = "hsla(" + calcComplementary() + ", " + S + "%, " + L + "%, " + A + ")";
+  /**
+   *
+   * @param {*} H
+   * @param {*} S
+   * @param {*} L
+   * @param {*} A
+   */
+  _displayComplementary: function(H, S, L, A) {
+    var H = H + 180 - 360;
+
+    this.complementary.style.background = "hsla(" + this._calcComplementary(H) + ", " + S + "%, " + L + "%, " + A + ")";
     this.complementary.innerHTML = "<div class='result_complementary'>" +
     "<div class='color_title'><strong>Complementary</strong></div>" +
       "<div class='color_result'>" +
         "<div class='color_result_item h'>" +
-          "<div class='color_result_value'>" + calcComplementary() + "</div> " +
+          "<div class='color_result_value'>" + this._calcComplementary(H) + "</div> " +
           "<div class='color_result_def'>H</div> " +
         "</div>" +
         "<div class='color_result_item s'>" +
@@ -220,14 +125,25 @@ var hslaPlayground = {
         "</div>" +
       "</div>" +
     "</div>";
+  },
 
-    // set triadic
-    this.triadic.style.background = "hsla(" + calcTriadic() + ", " + S + "%, " + L + "%, " + A + ")";
+  /**
+   *
+   * @param {*} H
+   * @param {*} S
+   * @param {*} L
+   * @param {*} A
+   */
+  _displayTriadic: function(H, S, L, A) {
+
+    var H = H + 120 - 360;
+
+    this.triadic.style.background = "hsla(" + this._calcTriadic(H) + ", " + S + "%, " + L + "%, " + A + ")";
     this.triadic.innerHTML = "<div class='result_complementary'>" +
     "<div class='color_title'><strong>Triadic</strong></div>" +
       "<div class='color_result'>" +
         "<div class='color_result_item h'>" +
-          "<div class='color_result_value'>" + calcTriadic() + "</div> " +
+          "<div class='color_result_value'>" + this._calcTriadic(H) + "</div> " +
           "<div class='color_result_def'>H</div> " +
         "</div>" +
         "<div class='color_result_item s'>" +
